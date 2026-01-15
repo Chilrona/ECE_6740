94625f1282cd086c2c68e4c61861b96cc7c73989
vanessabingham <a02274022@usu.edu>
2026-01-14 17:26:47 -0700

    Instruction Macros

---
+++ Lab2Assembler/Instruction_Macros.h
@@ -0,0 +1,73 @@
+#include "stdio.h"
+// OPCODES
+
+
+//Source Registers
+#define R1_SOURCE 0x00010000
+#define R2_SOURCE
+#define R3_SOURCE
+#define R4_SOURCE
+#define R5_SOURCE
+#define R6_SOURCE
+#define R7_SOURCE
+#define R8_SOURCE
+#define R9_SOURCE
+#define R10_SOURCE
+#define R11_SOURCE
+#define R12_SOURCE
+#define R13_SOURCE
+#define R14_SOURCE
+#define R15_SOURCE
+#define R16_SOURCE
+#define R17_SOURCE
+#define R18_SOURCE
+#define R19_SOURCE
+#define R20_SOURCE
+#define R21_SOURCE
+#define R22_SOURCE
+#define R23_SOURCE
+#define R24_SOURCE
+#define R25_SOURCE
+#define R26_SOURCE
+#define R27_SOURCE
+#define R28_SOURCE
+#define R29_SOURCE
+#define R30_SOURCE
+#define R31_SOURCE
+
+// Destination Registers
+#define R1_DEST 0x00200000
+#define R2_DEST
+#define R3_DEST
+#define R4_DEST
+#define R5_DEST
+#define R6_DEST
+#define R7_DEST
+#define R8_DEST
+#define R9_DEST
+#define R10_DEST
+#define R11_DEST
+#define R12_DEST
+#define R13_DEST
+#define R14_DEST
+#define R15_DEST
+#define R16_DEST
+#define R17_DEST
+#define R18_DEST
+#define R19_DEST
+#define R20_DEST
+#define R21_DEST
+#define R22_DEST
+#define R23_DEST
+#define R24_DEST
+#define R25_DEST
+#define R26_DEST
+#define R27_DEST
+#define R28_DEST
+#define R29_DEST
+#define R30_DEST
+#define R31_DEST
+
+//Field clear bits
+#define RD_CLEAR
+#define RS_CLEAR 0xFFE0FFFF
