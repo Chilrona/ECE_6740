4a174acac14a278485d34d6ecee6140fd79c02ce
vanessabingham <a02274022@usu.edu>
2026-02-02 11:28:25 -0700

    Wrote registers.vhd

--- Lab4Decode/registers.vhd
+++ Lab4Decode/registers.vhd
@@ -7,9 +7,9 @@ ENTITY single_clock_ram IS
     PORT (
         clk: IN STD_LOGIC;
         data: IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-        write_address: IN INTEGER RANGE 0 to 31;
-        read_address_1: IN INTEGER RANGE 0 to 31;
-        read_address_2: IN INTEGER RANGE 0 to 31;
+        write_address: IN std_logic_vector(25 downto 21);
+        read_address_1: IN std_logic_vector(20 downto 16);
+        read_address_2: IN std_logic_vector(15 downto 11);
         we: IN STD_LOGIC;
         q_1: OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
         q_2: OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
@@ -24,10 +24,10 @@ BEGIN
     BEGIN
         IF (rising_edge(clock)) THEN
             IF (we = '1') THEN
-                ram_block(write_address) := data;
+                ram_block(to_integer(write_address)) := data;
             END IF;
-            q_1 <= ram_block(read_address_1); 
-            q_2 <= ram_block(read_address_2);
+            q_1 <= ram_block(to_integer(read_address_1)); 
+            q_2 <= ram_block(to_integer(read_address_2));
             -- VHDL semantics imply that q doesn't get data 
             -- in this clock cycle
         END IF;
