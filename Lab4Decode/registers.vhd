e495b81abd6642294755d15ed1fc687dfd47a107
Amaya Francom <a02395949@aggies.usu.edu>
2026-02-04 15:12:10 -0700

    Change a bunch of stuff

--- Lab4Decode/registers.vhd
+++ Lab4Decode/registers.vhd
@@ -19,11 +19,12 @@ end registers;
 architecture rtl of registers is
     
 begin
+	
     process (clk)
-    variable ram_block: MEM;
+    variable ram_block: MEM:=(others=>(others=>'0'));
     begin
         if (rising_edge(clk)) THEN
-            if (we = '1') THEN
+            if (we = '1') then -- and not(write_address = "00000") THEN
                 ram_block(to_integer(unsigned(write_address))) := data;
             end if;
             q_1 <= ram_block(to_integer(unsigned(read_address_1))); 
