f22bac32b167f9d849051b1255b5bd97e821df0e
amayafrancom <amayafrancom@gmail.com>
2026-03-02 16:00:13 -0700

    added some comments to PC_mux

--- Lab7Hazards/PC_mux.vhd
+++ Lab7Hazards/PC_mux.vhd
@@ -10,6 +10,8 @@ entity PC_mux is
             pc : in std_logic_vector(9 downto 0);
             q_1 : in std_logic_vector(31 downto 0);
             op1 : out std_logic_vector(31 downto 0)
+				--add the ALU result coming out of execute as one of the mux inputs
+				--add something other stuff but IDK what 
 		);
 						
 	end entity PC_mux;	
@@ -21,6 +23,7 @@ entity PC_mux is
             if opcode = JAL or opcode = JALR then
                 op1 <= std_logic_vector(resize(unsigned(pc), 32));
             else
+				--need to add an if statement for checking data hazards
                 op1 <= q_1;
             end if;
         end process;
