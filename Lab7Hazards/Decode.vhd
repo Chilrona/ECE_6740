cb72119f958b67f9ae5c871e4319650bfefc06a5
Jackson Gardner <jackson.a.gardner@usu.edu>
2026-03-03 12:19:30 -0700

    edited fetch and decode to be able to flush and stall

--- Lab7Hazards/Decode.vhd
+++ Lab7Hazards/Decode.vhd
@@ -14,6 +14,9 @@ use work.opcode_package.all;
 
 			instruction_in : in std_logic_vector(31 downto 0);
 			instruction_out : out std_logic_vector(31 downto 0) := (others=>'0') ;
+			
+			tag_for_flush : in std_logic;
+			stall : in std_logic;
 
 			pc_in : in std_logic_vector(9 downto 0);
 			pc_out : out std_logic_vector(9 downto 0) := (others=>'0');
@@ -27,16 +30,20 @@ use work.opcode_package.all;
 					
 	architecture Behavioral of decode is     
 	    
+		signal instruction_in_ : std_logic_vector(31 downto 0);
+
 	begin
 	
+	instruction_in_ <= (others=>'0') when tag_for_flush = '1' else instruction_in;
+
 	R: entity work.registers 
 	port map
 	(
 		clk => clk,
         data => data,
         write_address => wr_addr,
-        read_address_1 => instruction_in(20 downto 16),
-        read_address_2 => instruction_in(15 downto 11),
+        read_address_1 => instruction_in_(20 downto 16),
+        read_address_2 => instruction_in_(15 downto 11),
         we => we,
         q_1 => q_1,
         q_2 => q_2
@@ -47,16 +54,20 @@ use work.opcode_package.all;
     (
         rst_l => rst_l,
         clk => clk,
-        opcode => instruction_in(31 downto 26),
-        imm => instruction_in(15 downto 0),
+        opcode => instruction_in_(31 downto 26),
+        imm => instruction_in_(15 downto 0),
         imm_extended => imm_extended
     );
 
 	pass_along: process (clk)
 	begin
 		if rising_edge(clk) then
-			instruction_out <= instruction_in;
-			pc_out <= pc_in;
+			if stall = '0' then
+				instruction_out <= instruction_in_;
+				pc_out <= pc_in;
+			else
+				instruction_out <= instruction_out;
+				pc_out <= pc_out;
 		end if;
 	end process;
 				
--- Lab7Hazards/fetch.vhd
+++ Lab7Hazards/fetch.vhd
@@ -10,6 +10,10 @@ use work.opcode_package.all;
 			clk : in std_logic;
 			jump_addr : in std_logic_vector(9 downto 0);
 			sel_jump : in std_logic;
+
+			tag_for_flush : in std_logic;
+			stall : in std_logic;
+
 			pc : buffer std_logic_vector(9 downto 0);
 			instruction : out std_logic_vector(31 downto 0)
 		);
@@ -21,6 +25,9 @@ use work.opcode_package.all;
 		signal next_pc : std_logic_vector(9 downto 0):=(others=>'0');
 		signal add_out : std_logic_vector(9 downto 0):=(others=>'0');
 		signal std_pc : std_logic_vector(9 downto 0):=(others=>'0');
+
+		signal rom_instruction : std_logic_vector(31 downto 0):=(others=>'0');
+		signal tag_for_flush_1d : std_logic;
         
 	
 	begin
@@ -30,7 +37,7 @@ use work.opcode_package.all;
 	(
 		address => std_pc,
 		clock	=> clk,
-		q	=> instruction
+		q	=> rom_instruction
 	);
 		std_pc <= pc;
 		
@@ -38,13 +45,20 @@ use work.opcode_package.all;
 
         next_pc <= ((9 downto 0 => sel_jump) and jump_addr)or((9 downto 0=>not(sel_jump)) and add_out);
 
+		instruction <= (others => '0') when tag_for_flush_1d = '1' else rom_instruction;
+
         process(clk, rst_l)
         begin 
 		  
 			if(rst_l = '0') then--checking for reset in pc
 				 pc <= (others=>'0');
 			elsif rising_edge(clk) then
-				 pc <=next_pc;
+				tag_for_flush_1d <= tag_for_flush;
+				if stall = '0' then
+					pc <=next_pc;
+				else
+					pc <= pc;
+				end if;
 			end if;
         end process;
 				
