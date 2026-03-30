ad78562da2629c3e5190221e14a2fc78e8394be2
amaya_francom <amayafrancom@gmail.com>
2026-03-30 00:41:31 -0600

    final project folder created

---
+++ FinalProject/DLX.vhd
@@ -0,0 +1,243 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.opcode_package.all;
+
+	entity DLX is
+		port 
+		(
+			KEY : in std_logic_vector(1 downto 0);
+			ADC_CLK_10 : in std_logic;
+			MAX10_CLK1_50 : in std_logic;
+			MAX10_CLK2_50 : in std_logic;
+			GPIO : inout std_logic_vector(35 downto 0)
+		);
+						
+	end entity DLX;	
+					
+	architecture Behavioral of DLX is  
+
+		--clk and rst signals
+		signal clk : std_logic;
+		signal rst_l : std_logic;
+		
+		signal pc_decode : std_logic_vector(9 downto 0);
+		signal pc_execute : std_logic_vector(9 downto 0);
+		
+		signal instruction_decode : std_logic_vector(31 downto 0);
+		signal instruction_execute : std_logic_vector(31 downto 0);
+		signal instruction_mem : std_logic_vector(31 downto 0);
+		signal instruction_wb : std_logic_vector(31 downto 0);
+		
+		signal q_1 : std_logic_vector (31 DOWNTO 0) := (others=>'0');
+      signal q_2 : std_logic_vector(31 downto 0) := (others=>'0');
+		signal q_2_mem : std_logic_vector(31 downto 0) := (others=>'0');
+		signal op1 : std_logic_vector(31 downto 0);
+		
+		signal imm_extended : std_logic_vector(31 downto 0);
+		signal jump_addr_final : std_logic_vector(9 downto 0);
+		signal branch_addr : std_logic_vector(9 downto 0);
+      signal jump_addr : std_logic_vector(9 downto 0);
+		signal take_jump : std_logic;
+		signal sel_jump : std_logic;
+		signal take_branch : std_logic;
+		
+		
+		signal alu_result : std_logic_vector(31 downto 0);
+		signal alu_result_wb : std_logic_vector(31 downto 0);
+		
+		signal ram_data : std_logic_vector(31 downto 0);
+		signal reg_data : std_logic_vector (31 downto 0);
+		
+		signal wr_addr : std_logic_vector(4 downto 0 );
+
+		--write enable signals
+		signal reg_we : std_logic;
+		signal ram_we : std_logic;
+		signal stall : std_logic;
+
+		signal stall_final : std_logic;
+		
+		--the flushes
+		signal flush_fetch : std_logic;
+		signal flush_decode : std_logic;
+		signal flush_execute :std_logic;
+
+		signal flush_decode_final : std_logic;
+
+		-- print signals
+		signal print_stall : std_logic;
+		signal print_flush_decode : std_logic;
+		-- signal tx : std_logic;
+		
+		-- scan signals
+		signal got_data : std_logic_vector(31 downto 0);
+		signal in_buf_empty : std_logic;
+		
+		--pll signals
+		signal areset		: STD_LOGIC := '0';
+		signal c0		: STD_LOGIC :='0'; --153600 Hz
+		signal c1		: STD_LOGIC :='0'; --115.2 kHz 
+		signal locked : std_logic;
+
+
+	
+	begin
+	
+	--setting up the clk and rst and tx
+	
+	rst_l <= KEY(0) and locked;
+	--rst_l <= KEY(0);
+	clk <= MAX10_CLK1_50;
+	areset <= not (KEY(0));
+	
+	FETCH: entity work.fetch 
+	port map
+	(
+		rst_l => rst_l,
+		clk => clk,
+      jump_addr => jump_addr_final,
+      sel_jump => sel_jump,
+		tag_for_flush => flush_fetch,
+		stall => stall_final,
+      pc => pc_decode,
+      instruction => instruction_decode
+	);
+
+    DECODE: entity work.decode
+	port map
+	(
+		rst_l => rst_l,
+      clk => clk,
+      data => reg_data,
+      instruction_in => instruction_decode,
+		instruction_out => instruction_execute,
+		tag_for_flush => flush_decode_final,
+		pc_in => pc_decode,
+		pc_out => pc_execute,
+		we => reg_we,
+		wr_addr => wr_addr,
+      q_1 => q_1,
+      q_2 => q_2,
+		imm_extended => imm_extended
+	);
+
+	EXECUTE: entity work.execute
+	port map
+	(
+		rst_l => rst_l,
+		clk => clk,
+		q_1 => q_1,
+		q_2 => q_2,
+		imm_extended => imm_extended,
+		pc_in => pc_execute,
+		instruction_in => instruction_execute,
+		instruction_in_wb => instruction_wb,
+		got_data => got_data,
+		reg_data => reg_data,
+		tag_for_flush => flush_execute,
+		instruction_out => instruction_mem,
+		alu_result => alu_result,
+		branch_addr => branch_addr,
+		take_branch => take_branch,
+		ram_we => ram_we,
+		q_2_out => q_2_mem,
+		op1_out => op1
+	);
+	MEMORY: entity work.memory
+	port map
+	(
+		rst_l => rst_l,
+		clk => clk,
+		instruction_in => instruction_mem,
+		instruction_out => instruction_wb,
+		q_2 => q_2_mem,
+		alu_result => alu_result,
+		ram_we => ram_we,
+		ram_data => ram_data,
+		alu_result_wb => alu_result_wb
+	);
+
+	WB : entity work.write_back
+	port map
+	(
+        instruction_wb => instruction_wb,
+        ram_data => ram_data,
+        alu_result_wb => alu_result_wb,
+        wr_addr => wr_addr,
+        reg_data => reg_data,
+        reg_we => reg_we
+	);
+	
+	
+	OOO : entity work.OVERSEER 
+    port map
+    (
+        clk => clk,
+
+        instruction_decode => instruction_decode,
+        instruction_execute => instruction_execute,
+        instruction_wb => instruction_wb,
+
+        take_branch => take_branch,
+		  in_buf_empty => in_buf_empty,
+
+        flush_fetch => flush_fetch,
+        flush_decode => flush_decode,
+        flush_execute => flush_execute,
+
+        take_jump => take_jump,
+        jump_addr => jump_addr,
+
+        stall => stall
+    );
+	 
+	 FSM: entity work.print_FSM
+		port map
+		(
+         rst_l => rst_l,
+         clk => clk,
+			opcode_FSM => instruction_mem(31 downto 26),
+			op1 => op1,
+			print_stall => print_stall,
+			print_flush_decode => print_flush_decode,
+			tx => GPIO(1), 
+			uart_clk => c1
+		);
+		
+		SCAN : entity work.scan_FSM
+		port map 
+		(
+         rst_l => rst_l,
+         clk => clk,
+			uart_clk =>  c1, --115.2kHz
+			uart_samp_clk => c0, --921.6 kHz
+         opcode_scan => instruction_decode(31 downto 26),
+			rx => GPIO(0),
+
+			data_out => got_data,
+			in_buff_empty => in_buf_empty
+		);
+		
+		
+		PLLTX: entity work.pll 
+		PORT MAP
+		(
+			areset=> areset,
+			inclk0=>MAX10_CLK1_50,
+			c0=>c0,
+			c1=>c1,
+			locked=>locked
+		);
+	
+	 
+	 stall_final <= stall or print_stall;
+	 flush_decode_final <= flush_decode or print_flush_decode; 
+	
+	 jump_addr_final <= branch_addr when take_jump = '0' else jump_addr;
+	 sel_jump <= take_branch or take_jump;
+	 
+	
+
+				
+	end Behavioral;
---
+++ FinalProject/Execute.vhd
@@ -0,0 +1,121 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.opcode_package.all;
+
+	entity execute is
+		port 
+		(
+            rst_l : in std_logic;
+            clk : in std_logic;
+            q_1 : in std_logic_vector (31 downto 0);
+            q_2 : in std_logic_vector(31 downto 0);
+            imm_extended : in std_logic_vector(31 downto 0);
+			 
+            pc_in : in std_logic_vector(9 downto 0);
+			 
+            instruction_in : in std_logic_vector(31 downto 0);
+            instruction_in_wb : in std_logic_vector(31 downto 0);
+				got_data : in std_logic_vector(31 downto 0);
+            reg_data : in std_logic_vector(31 downto 0);
+            tag_for_flush : in std_logic;
+
+			instruction_out : buffer std_logic_vector(31 downto 0) := (others=>'0');
+            alu_result : buffer std_logic_vector(31 downto 0);
+            branch_addr : out std_logic_vector(9 downto 0);
+			take_branch : out std_logic;
+            ram_we : out std_logic;
+            q_2_out : out std_logic_vector(31 downto 0);
+				op1_out : out std_logic_vector(31 downto 0) := (others=>'0')
+		);
+						
+	end entity execute;	
+					
+	architecture Behavioral of execute is     
+	
+    signal op1 : std_logic_vector(31 downto 0) := (others => '0');
+	 signal op2 : std_logic_vector(31 downto 0) := (others => '0');
+	 signal instruction_in_1 : std_logic_vector(31 downto 0);
+
+
+	begin
+	
+	instruction_in_1 <= (others=>'0') when (tag_for_flush = '1') else instruction_in;
+	
+        
+	MUX1 : entity work.PC_mux
+    port map
+    (
+		  opcode_exe => instruction_in_1(31 downto 26),
+		  opcode_mem => instruction_out(31 downto 26),
+		  opcode_wb => instruction_in_wb(31 downto 26),
+        rd_mem => instruction_out(25 downto 21),
+        rd_wb => instruction_in_wb(25 downto 21),
+        RS1 => instruction_in_1(20 downto 16), 
+        reg_data => reg_data,
+        alu_result => alu_result,
+        pc => pc_in,
+        q_1 => q_1,
+        op1 => op1
+    );
+	 
+
+    MUX2 : entity work.imm_mux
+    port map
+    (
+        opcode_exe => instruction_in_1(31 downto 26),
+		  opcode_mem => instruction_out(31 downto 26),
+		  opcode_wb => instruction_in_wb(31 downto 26), 
+        rd_mem => instruction_out(25 downto 21),
+        rd_wb => instruction_in_wb(25 downto 21),
+        RS2 => instruction_in_1(15 downto 11),
+        reg_data => reg_data,
+        alu_result => alu_result,
+        imm_ex => imm_extended,
+        q_2 => q_2,
+        op2 => op2
+    );
+    
+    ZEROS: entity work.zeros 
+	port map
+	(
+			rst_l => rst_l,
+			clk => clk,
+			jump_addr =>branch_addr,
+			sel_jump => take_branch,
+			op1 => op1,
+			op2 => op2,
+			instruction_execute => instruction_in_1
+	);
+
+	ALU: entity work.ALU
+    port map
+    (
+        rst_l => rst_l,
+        clk => clk,
+        opcode => instruction_in_1(31 downto 26),
+		  got_data => got_data,
+        op1 => op1,
+        op2 => op2,
+        alu_result => alu_result,
+        ram_we => ram_we
+    );
+	 
+	 pass_along: process(clk, rst_l)
+	 begin
+		if rst_l = '0' then
+			instruction_out <= (others=>'0');
+            q_2_out <= (others=>'0');
+				op1_out <= (others=>'0');
+		elsif rising_edge(clk) then
+				instruction_out <= instruction_in_1;
+				op1_out <= op1;
+            q_2_out <= q_2;
+		end if;
+	end process;
+	end Behavioral;
+	
+						
+						
+						
+						
